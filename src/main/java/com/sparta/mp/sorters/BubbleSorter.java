b53efc97ff06b8abc5bd36887155d108e1947302
MichalisPer <michalis_2012@hotmail.com>
2022-10-17 23:39:53 +0100

    Implementation of sortArray method in BubbleSorter class

--- src/main/java/com/sparta/mp/sorters/BubbleSorter.java
+++ src/main/java/com/sparta/mp/sorters/BubbleSorter.java
@@ -1,4 +1,24 @@
 package com.sparta.mp.sorters;
 
-public class BubbleSorter {
+public class BubbleSorter implements Sorter {
+    @Override
+    public int[] sortArray(int[] array) {
+        boolean isSorted;
+        do {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    performTriangleSwap(array, i);
+                    isSorted = false;
+                }
+            }
+        } while (!isSorted);
+        return array;
+    }
+
+    private static void performTriangleSwap(int[] sortedArray, int i) {
+        int temp = sortedArray[i];
+        sortedArray[i] = sortedArray[i + 1];
+        sortedArray[i + 1] = temp;
+    }
 }
