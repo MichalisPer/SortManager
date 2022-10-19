6c94d13310efef70ab713d8cc39e3dcb93e67389
MichalisPer <michalis_2012@hotmail.com>
2022-10-20 00:29:55 +0100

    Implementation of InsertionSorter algorithm

--- src/main/java/com/sparta/mp/sorters/InsertionSorter.java
+++ src/main/java/com/sparta/mp/sorters/InsertionSorter.java
@@ -1,8 +1,26 @@
 package com.sparta.mp.sorters;
 
-public class InsertionSorter implements Sorter{
+import java.util.Arrays;
+
+public class InsertionSorter implements Sorter {
     @Override
     public int[] sortArray(int[] array) {
-        return new int[0];
+        int[] arr = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    performTriangleSwap(arr, j);
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private static void performTriangleSwap(int[] arr, int j) {
+        int temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
     }
 }
